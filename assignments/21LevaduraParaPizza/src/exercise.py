c0ff61bbf911602cfe56450e513f13b1ad5e1bd9
alannybanda <89655009+alannybanda@users.noreply.github.com>
2021-08-27 13:57:42 -0500

    levadura

--- assignments/21LevaduraParaPizza/src/exercise.py
+++ assignments/21LevaduraParaPizza/src/exercise.py
@@ -1,6 +1,8 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    gramos = float(input("Dame la harina en gramos: "))
+    levadura = (50 * gramos) / 1000
+    print("Necesitas estos gramos de levadura:", levadura)
 
 
 if __name__ == '__main__':
