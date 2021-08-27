5af3aa51444760cefb3a93b85125414299d08969
alannybanda <89655009+alannybanda@users.noreply.github.com>
2021-08-27 13:54:11 -0500

    editorial

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,6 +1,8 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    palabras = int(input("Dame el número de palabras: "))
+    costo = ((palabras // 475) + 1) * 54
+    print("El costo de la publicación es:", costo)
 
 
 if __name__ == '__main__':
